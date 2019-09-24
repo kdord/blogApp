d094467cd3db4c165d7cca920799c9a5d868cc16
kdord <katya.koliada49@gmail.com>
2019-09-25 00:03:38 +0300

    blogs route added

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
 app.use(methodOverride("_method"))
 
+//require routes
+var blogRoutes 		= require("./routes/blogs.js")
+
 
 app.get("/", function(req, res){
 	res.render("landing")
 })
 
-app.get("/blogs", function(req, res){
-	res.render("blogs.ejs")
-})
+
+app.use(blogRoutes)
+
 
 app.listen(3000, function(){
 	console.log("Server has started")
