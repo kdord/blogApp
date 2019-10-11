83b2671d03921672ce3dcda594ca25a2e907ccd4
kdord <katya.koliada49@gmail.com>
2019-10-11 13:17:21 +0300

    move db credentials to env

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ var indexRoutes 	= require("./routes/index.js")
 var commentRoutes 	= require("./routes/comments.js")
 var userRoutes 		= require("./routes/user.js")
 
-
+var DBPASS = process.env.DBPASS 
+var DBUSERNAME = process.env.DBUSERNAME
 // seedDB() //seed the database
 
-mongoose.connect("mongodb://localhost/blog_app", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+mongoose.connect(`mongodb+srv://${DBUSERNAME}:${DBPASS})@cluster0-lwmoa.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
 	.then(() => console.log("connection succesful"))
 	.catch((err) => console.error(err))
 app.use(bodyParser.urlencoded({extended:true}))
