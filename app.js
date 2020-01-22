8ddba4f05d4a7b559e945149a326c7efac648d00
kdord <katya.koliada49@gmail.com>
2020-01-22 16:51:02 +0200

    fix bug

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
-var express  		= require("express"),
-	app 			= express(),
-	bodyParser		= require("body-parser"),
-	mongoose		= require("mongoose"),
-	methodOverride	= require("method-override"),
-	Post 			= require("./models/post.js"),
-	seedDB 			= require("./seeds.js"),
-	passport 		= require("passport"),
-	LocalStrategy 	= require("passport-local"),
-	session 		= require("express-session"),
-	User 			= require("./models/user.js"),
-	Comment 		= require("./models/comment.js"),
-	flash 			= require("connect-flash")
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  methodOverride = require('method-override'),
+  Post = require('./models/post.js'),
+  seedDB = require('./seeds.js'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  session = require('express-session'),
+  User = require('./models/user.js'),
+  Comment = require('./models/comment.js'),
+  flash = require('connect-flash');
 
 //require routes
-var postRoutes 		= require("./routes/posts.js")
-var indexRoutes 	= require("./routes/index.js")
-var commentRoutes 	= require("./routes/comments.js")
-var userRoutes 		= require("./routes/user.js")
+var postRoutes = require('./routes/posts.js');
+var indexRoutes = require('./routes/index.js');
+var commentRoutes = require('./routes/comments.js');
+var userRoutes = require('./routes/user.js');
 
-
-const localMongo = "mongodb://localhost/blog_app"
-const DBURL = process.env.DBURL
+const localMongo = 'mongodb://localhost/blog_app';
+const DBURL = process.env.DBURL;
 // seedDB() //seed the database
 
-
-mongoose.connect(DBURL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
-	.then(() => console.log("connection succesful"))
-	.catch((err) => console.error(err))
-app.use(bodyParser.urlencoded({extended:true}))
-app.set("view engine", "ejs")
-app.use(express.static(__dirname + "/public"))
-app.use(methodOverride("_method"))
-app.use(flash()) //for flash messages
-
-
-
+mongoose
+  .connect(DBURL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connection succesful'))
+  .catch(err => console.error(err));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(flash()); //for flash messages
 
 //PASSPORT CONFIG
-app.use(session({
-	secret: "secret text",
-	resave: false,
-	saveUninitialized: false
-}))
+app.use(
+  session({
+    secret: 'secret text',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user
-	res.locals.error = req.flash("error")
-	res.locals.success = req.flash("success")
-	next()
-})
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
 
+app.use('/', indexRoutes);
+app.use('/posts', postRoutes);
+app.use('/posts/:id/comments', commentRoutes);
+app.use(userRoutes);
 
-app.use("/", indexRoutes)
-app.use("/posts", postRoutes)
-app.use("/posts/:id/comments", commentRoutes)
-app.use(userRoutes)
-
-app.listen(process.env.PORT, function(){
-	console.log("Server has started")
-})
+app.listen(process.env.PORT, function() {
+  console.log('Server has started');
+});
